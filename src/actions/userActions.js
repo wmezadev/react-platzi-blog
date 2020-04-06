6a675d2ffd232bebd876e0915b8c9bedad321bfd
William Eduardo Meza <williameduardomeza@gmail.com>
2020-04-06 10:48:28 -0500

    feat: add try and catch, when fetch error for GET_ALL action dispatch

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -2,10 +2,14 @@ import axios from 'axios';
 import { GET_ALL } from '../types/userTypes';
 
 export const getAll = () => async (dispatch) => {
-    const resp = await axios.get(`https://jsonplaceholder.typicode.com/users`);
+    try {
+        const resp = await axios.get(`https://jsonplaceholder.typicode.com/users`);
+        dispatch({
+            type: GET_ALL,
+            payload: resp.data
+        })
+    } catch (error){
+        console.log("Error: ", error.message)
+    }
 
-    dispatch({
-        type: GET_ALL,
-        payload: resp.data
-    })
 }
