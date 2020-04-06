b543150c102ed429009eed183c85f29741f6197c
William Eduardo Meza <williameduardomeza@gmail.com>
2020-04-06 12:08:09 -0500

    feat: add posts component and pass an param by url

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,11 +3,13 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Menu from './Menu';
 import Users from './Users';
 import Tasks from './Tasks';
+import Posts from './Posts';
 
 const App = () => (
   <BrowserRouter>
     <Menu/>
     <main className="page-container">
+      <Route exact path="/posts/:key" component={Posts} />
       <Route exact path="/tasks" component={Tasks} />
       <Route exact path="/" component={Users} />
     </main>
--- src/components/Users/Table.jsx
+++ src/components/Users/Table.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const Table = ({ users }) => {
     const addRows = () => (
-        users.map(user => (
+        users.map((user, key) => (
           <tr key={user.id}>
-            <td>{user.name}</td>
-            <td>{user.email}</td>
-            <td>{user.website}</td>
+            <td>
+                {user.name}
+            </td>
+            <td>
+                {user.email}
+            </td>
+            <td>
+                {user.website}
+            </td>
+            <td>
+                <Link to={`/posts/${key}`}>
+                    <div className="eye-solid icon"></div>
+                </Link>
+            </td>
           </tr>
         ))
     );
