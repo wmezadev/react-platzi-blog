ea075b1b0093fe56f8c7058a88d34c86be52e374
William Eduardo Meza <williameduardomeza@gmail.com>
2020-04-06 12:13:33 -0500

    feat: re-fetch with userAction when acces direct to posts url

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
+import * as usersActions from '../../actions/userActions';
 class Posts extends Component {
+
+    componentDidMount() {
+        if(!this.props.users.length) {
+            this.props.getAll();
+        }
+    }
+
     render() {
         return (
             <div>
+                <h1> Post from </h1>
                 { this.props.match.params.key }
             </div>
         );
     }
 }
 
-export default Posts;
+const mapStateToProps = (reducers) => {
+    return reducers.userReducer;
+};
+
+export default connect(mapStateToProps, usersActions)(Posts);
